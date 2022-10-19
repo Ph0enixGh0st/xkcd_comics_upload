804a2f5f93343b645aef2b6c36e4b224196c00e9
Ph0enixGh0st <mikhail.ostanin@gmail.com>
2022-10-19 22:07:38 +0300

    Variabes updated
    Variabes updated

--- comics_upload.py
+++ comics_upload.py
@@ -6,31 +6,31 @@ from dotenv import load_dotenv
 from pathlib import Path
 
 
-def download_random_comics():
+def download_random_cartoon():
 
-    comics_count_url = f'https://xkcd.com/info.0.json'
-    comics_limit = requests.get(comics_count_url)
-    comics_limit.raise_for_status()
-    comics_limit = comics_limit.json()
-    comics_random_number = random.randint(1, comics_limit['num'])
+    cartoon_count_url = f'https://xkcd.com/info.0.json'
+    cartoon_limit = requests.get(cartoon_count_url)
+    cartoon_limit.raise_for_status()
+    cartoon_limit = cartoon_limit.json()
+    cartoon_random_number = random.randint(1, cartoon_limit['num'])
 
-    comics_url = f'https://xkcd.com/{comics_random_number}/info.0.json'
-    comics_pic = requests.get(comics_url)
-    comics_pic.raise_for_status()
-    comics_pic = comics_pic.json()
-    comics_pic_comment = comics_pic['alt']
+    cartoon_url = f'https://xkcd.com/{cartoon_random_number}/info.0.json'
+    cartoon_pic = requests.get(cartoon_url)
+    cartoon_pic.raise_for_status()
+    cartoon_pic = cartoon_pic.json()
+    cartoon_pic_comment = cartoon_pic['alt']
 
-    comics_download_link = comics_pic['img']
-    comics_download_link = requests.get(comics_download_link)
-    comics_download_link.raise_for_status()
+    cartoon_download_link = cartoon_pic['img']
+    cartoon_download_link = requests.get(cartoon_download_link)
+    cartoon_download_link.raise_for_status()
 
-    with open('comics.png', 'wb') as file:
-        file.write(comics_download_link.content)
+    with open('cartoon.png', 'wb') as file:
+        file.write(cartoon_download_link.content)
 
-    return comics_pic_comment
+    return cartoon_pic_comment
 
 
-def upload_comics_to_vk_server(vk_version, vk_access_token, vk_group_id):
+def upload_cartoon_to_vk_server(vk_version, vk_access_token, vk_group_id):
 
     server_upload_url = 'https://api.vk.com/method/photos.getWallUploadServer'
     payload = {
@@ -38,56 +38,55 @@ def upload_comics_to_vk_server(vk_version, vk_access_token, vk_group_id):
         'access_token': f'{vk_access_token}'
     }
 
-    comics_upload_vk_url = requests.get(server_upload_url, params=payload)
-    comics_upload_vk_url.raise_for_status()
-    comics_upload_vk_url = comics_upload_vk_url.json()
-    comics_upload_vk_url = comics_upload_vk_url['response']['upload_url']
+    cartoon_upload_vk_url = requests.get(server_upload_url, params=payload)
+    cartoon_upload_vk_url.raise_for_status()
+    cartoon_upload_vk_url = cartoon_upload_vk_url.json()
+    cartoon_upload_vk_url = cartoon_upload_vk_url['response']['upload_url']
 
-    with open('comics.png', 'rb') as file:
+    with open('cartoon.png', 'rb') as file:
         files = {
             'v': f"{vk_version}",
             'group_id ': f"{vk_group_id}",
             'photo': file,
         }
-        comics_server_upload = requests.post(comics_upload_vk_url, files=files)
-        comics_server_upload.raise_for_status()
-        comics_server_upload = comics_server_upload.json()
+        cartoon_server_upload = requests.post(cartoon_upload_vk_url, files=files)
+        cartoon_server_upload.raise_for_status()
+        cartoon_server_upload = cartoon_server_upload.json()
 
-        return comics_server_upload
+        return cartoon_server_upload
 
 
-def save_and_post_on_vk_wall(vk_access_token, comics_server_upload, vk_version, vk_group_id, vk_user_id, comics_pic_comment):
+def save_and_post_on_vk_wall(vk_access_token, cartoon_server_upload, vk_version, vk_group_id, vk_user_id, cartoon_pic_comment):
 
-    with open('comics.png', 'rb') as file:
-        url = 'https://api.vk.com/method/photos.saveWallPhoto'
-        params = {
+    url = 'https://api.vk.com/method/photos.saveWallPhoto'
+    params = {
         'access_token': f'{vk_access_token}',
-        'hash': f"{comics_server_upload['hash']}",
+        'hash': f"{cartoon_server_upload['hash']}",
         'v': f"{vk_version}",
-        'photo':f"{comics_server_upload['photo']}",
+        'photo':f"{cartoon_server_upload['photo']}",
         'group_id ': f"{vk_group_id}",
         'user_id': f"{vk_user_id}",
-        'server': f"{comics_server_upload['server']}"
-        }
+        'server': f"{cartoon_server_upload['server']}"
+    }
 
-        comics_server_save = requests.post(url, params=params)
-        comics_server_save.raise_for_status()
-        comics_server_save = comics_server_save.json()
+    cartoon_server_save = requests.post(url, params=params)
+    cartoon_server_save.raise_for_status()
+    cartoon_server_save = cartoon_server_save.json()
 
     vk_wall_post_url = 'https://api.vk.com/method/wall.post'
     params = {
-    'v': f"{vk_version}",
-    'access_token': f'{vk_access_token}',
-    'owner_id': f"{comics_server_save['response'][0]['owner_id']}",
-    'from_group': '1',
-    'message': f"{comics_pic_comment}",
-    'attachments': f"photo{comics_server_save['response'][0]['owner_id']}_{comics_server_save['response'][0]['id']}",
+        'v': f"{vk_version}",
+        'access_token': f'{vk_access_token}',
+        'owner_id': f"{cartoon_server_save['response'][0]['owner_id']}",
+        'from_group': '1',
+        'message': f"{cartoon_pic_comment}",
+        'attachments': f"photo{cartoon_server_save['response'][0]['owner_id']}_{cartoon_server_save['response'][0]['id']}",
     }
 
     vk_wall_post = requests.post(vk_wall_post_url, params=params)
     vk_wall_post.raise_for_status()
 
-    os.remove('comics.png')
+    os.remove('cartoon.png')
 
 
 def main():
@@ -98,10 +97,10 @@ def main():
     vk_user_id = os.environ['VK_USER_ID']
     vk_version = os.environ['VK_VERSION']
 
-    comics_pic_comment = download_random_comics()
-    comics_server_upload = upload_comics_to_vk_server(vk_version, vk_access_token, vk_group_id)
+    cartoon_pic_comment = download_random_cartoon()
+    cartoon_server_upload = upload_cartoon_to_vk_server(vk_version, vk_access_token, vk_group_id)
 
-    save_and_post_on_vk_wall(vk_access_token, comics_server_upload, vk_version, vk_group_id, vk_user_id, comics_pic_comment)
+    save_and_post_on_vk_wall(vk_access_token, cartoon_server_upload, vk_version, vk_group_id, vk_user_id, cartoon_pic_comment)
 
 
 if __name__ == '__main__':
